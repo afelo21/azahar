ff6c25e674e4a7c10f3b10678e7b1adcbe8081d6
Juan Arbelaez <juan.arbelaez316@gmail.com>
2016-11-08 23:19:55 -0500

    Correccion de defectos de conexion
    Correccion en crearVariedad.php

--- crearVariedad.php
+++ crearVariedad.php
@@ -3,24 +3,47 @@
 <body>
 
 <?php
-	$idVariedad = $_POST['idVariedad'];
-	$nombre = $_POST['Nombre'];
+
+try {
+	include 'conect.php';
+	if($conexion = mysql_connect($host, $user, $pwd)){
+		try{
+			if(mysql_select_db($db,$conexion)){
+
+				//Codigo a ejecutar
+
+				$idVariedad = $_POST['idVariedad'];
+				$nombre = $_POST['Nombre'];
 	
 
-	$verifica = strlen($idVariedad) * strlen($nombre);
+				$verifica = strlen($idVariedad) * strlen($nombre);
 
-	if ($verifica>0) {
-		    include 'conect.php';			
-			$conexion = mysql_connect($host, $user, $pwd) or die ("Error de conexion.");
-			mysql_select_db($db,$conexion) or die ("no se pudo conectar a la bd");
-			mysql_query("INSERT INTO variedades VALUES ('$idVariedad','$nombre')");
-			echo "Se ha guardado exitosamente";
+				if ($verifica>0) {
+		    
+					mysql_query("INSERT INTO variedades VALUES ('$idVariedad','$nombre')");
+					echo "Se ha guardado exitosamente";
 
-		}	
-		else {
-			echo "<b>"." (*) Por favor ingrese todos los valores requeridos"."</b>";
+				}	
+				else {
+					echo "<b>"." (*) Por favor ingrese todos los valores requeridos"."</b>";
+				}
+
+			}
+			else{
+				throw new Exception("No se puede conectar a la BD");
+			}
 		}
-	
+		catch(Exception $e1){
+			echo $e1->getMessage();
+		}
+	}
+	else{
+		throw new Exception("No se puede conectar");
+	}	
+}
+catch(Exception $e2){
+	echo $e2->getMessage();
+}
 
 ?>
 
